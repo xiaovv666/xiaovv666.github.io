0c7088c2d5af576d80d0e2b92a370444eaed618b
xiaovv666 <2054802894@qq.com>
2024-09-27 21:51:35 +0800

    Site updated: 2024-09-27 21:51:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/27/文章/"];function toRandomPost(){
+var posts=["2024/09/27/我的第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
